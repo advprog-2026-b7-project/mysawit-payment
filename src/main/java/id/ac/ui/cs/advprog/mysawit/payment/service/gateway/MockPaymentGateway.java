d4996597d4b38454e89a16d358403cdc75c2ef93
vidiaqonita <qonitavidia@gmail.com>
2026-04-14 21:41:25 +0700

    fix: replace System.out.println with logger in MockPaymentGateway

--- src/main/java/id/ac/ui/cs/advprog/mysawit/payment/service/gateway/MockPaymentGateway.java
+++ src/main/java/id/ac/ui/cs/advprog/mysawit/payment/service/gateway/MockPaymentGateway.java
@@ -2,12 +2,16 @@ package id.ac.ui.cs.advprog.mysawit.payment.service.gateway;
 
 import org.springframework.stereotype.Service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Service
 public class MockPaymentGateway implements PaymentGateway {
+    private static final Logger logger = LoggerFactory.getLogger(MockPaymentGateway.class);
+
     @Override
     public boolean processPayment(Double amount, String destinationAccount) {
-        System.out.println(">>> Mock Gateway: Processing Rp"
-                + amount + " to " + destinationAccount);
+        logger.info("Mock Gateway: Processing Rp{} to {}", amount, destinationAccount);
         return true;
     }
 }
