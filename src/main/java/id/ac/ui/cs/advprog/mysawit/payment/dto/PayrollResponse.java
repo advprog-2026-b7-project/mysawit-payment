e16fb25a82b99266276fb105a246a8fde6ecab69
Rekkin99 <farrellbagoes04@gmail.com>
2026-05-08 19:29:21 +0700

    feat: add api for wage settings

--- src/main/java/id/ac/ui/cs/advprog/mysawit/payment/dto/PayrollResponse.java
+++ src/main/java/id/ac/ui/cs/advprog/mysawit/payment/dto/PayrollResponse.java
@@ -22,7 +22,24 @@ public class PayrollResponse {
     private String payrollType;
     private String description;
     private LocalDateTime createdAt;
+    private CalculationDetailsDTO calculationDetails;
 
     public PayrollResponse() {
+    }
+
+    public PayrollResponse(UUID id, String workerId, String workerName, Double amount,
+                          String status, String referenceId, LocalDate tanggal,
+                          String payrollType, String description, LocalDateTime createdAt) {
+        this.id = id;
+        this.workerId = workerId;
+        this.workerName = workerName;
+        this.amount = amount;
+        this.status = status;
+        this.referenceId = referenceId;
+        this.tanggal = tanggal;
+        this.payrollType = payrollType;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.calculationDetails = null;
     }
 }
